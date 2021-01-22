60e512693ce3860f66b639ff522a90f7f818806f
Rakesh <sairakesh217@gmail.com>
2021-01-22 12:24:02 -0500

    Login Screen

--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,8 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'login',
   templateUrl: './login.html',
   styleUrls: ['./login.css'],
 })
-export class LoginComponent {}
+export class LoginComponent {
+  @Input() emailID: String;
+  @Input() password: String;
+  onSubmit() {
+    window.alert('Email: ' + this.emailID + ' Password: ' + this.password);
+  }
+  onClear() {
+    this.emailID = '';
+    this.password = '';
+  }
+}
